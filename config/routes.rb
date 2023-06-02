0c3e14ef114f3a1eb35b04c855fa61fb7ca72b27
Russell Hallowes-Smith <russellsmith6@gmail.com>
2023-06-02 15:20:52 +0100

    need to add some more features
    exit
    qq
    q
    q

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
